1acab3bb1c1d1293960c6a4e06d2eef575a9a6da
Kurniawan Adhi Wijaya <koadhyow@gmail.com>
2025-06-08 15:11:20 +0700

    Show Items

--- mern-pos/client/src/pages/Homepage.js
+++ mern-pos/client/src/pages/Homepage.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 
+import axios from "axios";
+import { Col, Row } from "antd";
+import Item from "../components/Item";
+
 function Homepage() {
+  const [itemsData, setItemsData] = useState([]);
+  const getAllItems = () => {
+    axios
+      .get("/api/items/get-all-items")
+      .then((res) => setItemsData(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getAllItems();
+  });
   return (
     <DefaultLayout>
-      <h1>Homepage</h1>
+      <Row>
+        {itemsData.map((item) => {
+          return (
+            <Col span={6}>
+              <Item item={item} />
+            </Col>
+          );
+        })}
+      </Row>
     </DefaultLayout>
   );
 }
